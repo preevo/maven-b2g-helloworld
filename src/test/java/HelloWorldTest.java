ac7cd3b473dd311b8f7b670000ec23c9c60ffbe3
preevo <preevo@ymail.com>
2023-05-21 09:35:20 -0400

    initial checkin of helloworld files

---
+++ src/test/java/HelloWorldTest.java
@@ -0,0 +1,9 @@
+public class HelloWorldTest {
+    public void testHello() {
+        System.out.println("TEST -- Helloo....");
+    }
+
+    public void testWorld() {
+        System.out.println("TEST -- World!!!!");
+    }
+}
